40d0fda3ea3470abe24edf1d37bd2cdc9ca01039
Joseph Converse <jpc6459@psu.edu>
2023-02-11 19:18:03 -0500

    CyberReact

--- src/App.js
+++ src/App.js
@@ -20,6 +20,26 @@ function App() {
   <button class="btnCT">Change Title</button>
   <button  class="btnBG">Change Background Color</button>
   
+
+  <button id="duplicate" class="outsideBtn">Duplicate</button>
+<button id="bg" class="outsideBtn">Background</button>
+<button id="header" class="outsideBtn">Heading</button>
+<button id="delete" class="outsideBtn">Delete</button>
+
+<div class="card">
+  <h1 class="title">Chad of Cyber IST</h1>
+
+  <img class="giaImg" src="https://media.discordapp.net/attachments/963095262363017246/1020131830323744788/unknown.png?width=468&height=468" alt="Professor Giacobe"/>
+  
+<div class="textbox">
+  <details>
+    <summary class="haxbtn">Details</summary>
+    <p class="description pScale">Professor Giacobe may look like a simple man but perceptions can be misleading. Underneath the facade of a simple college of IST professor lies the holiness of an <strong>IST GOD</strong>!</p>
+  </details>
+
+  </div>
+</div>
+
   </div>
   
 </>
